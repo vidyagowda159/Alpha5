852a048ee286a8e7c0fbba3406e047d66f167098
vidyagowda159 <vidyagowda005@gmail.com>
2022-05-01 10:07:19 +0530

    dictionary and sets files added

---
+++ dictionary.py
@@ -0,0 +1,158 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+d = {}
+type(d)
+<class 'dict'>
+d = dict()
+type(d)
+<class 'dict'>
+
+d = {"a": 1, "b": 2}
+d = dict({"a": 1, "b": 2})
+d = dict(a=1, b=2)
+d
+{'a': 1, 'b': 2}
+
+d1 = dict([("a", 1), ("b", 2), ("c", 3)])
+d1
+{'a': 1, 'b': 2, 'c': 3}
+
+len(d1)
+3
+
+d = {"a": 1, "b": 2, "a": 3}
+d
+{'a': 3, 'b': 2}
+
+
+d1 = {10: "hai"}
+d1
+{10: 'hai'}
+
+d2 = dict(10="hai")
+SyntaxError: expression cannot contain assignment, perhaps you meant "=="?
+
+d2 = {26, 1: "Republic day"}
+SyntaxError: invalid syntax
+d2 = {[26, 1]: "Republic day"}
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    d2 = {[26, 1]: "Republic day"}
+TypeError: unhashable type: 'list'
+d2 = {(26, 1): "Republic day"}
+d2
+{(26, 1): 'Republic day'}
+
+
+places = {"bangalore": 25, "Mysore": 35, "chennai": 30}
+
+places["bangalore"]
+25
+places["chennai"]
+30
+places["chennai_city"]
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    places["chennai_city"]
+KeyError: 'chennai_city'
+
+places.get("chennai")
+30
+places.get("chennai_city")
+places.get("chennai_city", "No key")
+'No key'
+places.get("chennai", "No key")
+30
+
+places.keys()
+dict_keys(['bangalore', 'Mysore', 'chennai'])
+places.values()
+dict_values([25, 35, 30])
+places.items()
+dict_items([('bangalore', 25), ('Mysore', 35), ('chennai', 30)])
+
+# adding key - value pairs to dictionary
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30}
+places["Kolkata"] = 40
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30, 'Kolkata': 40}
+
+places.update({"Punjab": 36})
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30, 'Kolkata': 40, 'Punjab': 36}
+places.update(Punjab = 38)
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30, 'Kolkata': 40, 'Punjab': 38}
+
+places.setdefault("kerala")
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30, 'Kolkata': 40, 'Punjab': 38, 'kerala': None}
+places.setdefault("kerala", 10)
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30, 'Kolkata': 40, 'Punjab': 38, 'kerala': None}
+places.setdefault("cochin", 10)
+10
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30, 'Kolkata': 40, 'Punjab': 38, 'kerala': None, 'cochin': 10}
+
+
+
+l = ["a", "b", "c"]
+tuple(l)
+('a', 'b', 'c')
+set(l)
+{'a', 'c', 'b'}
+dict(l)
+Traceback (most recent call last):
+  File "<pyshell#67>", line 1, in <module>
+    dict(l)
+ValueError: dictionary update sequence element #0 has length 1; 2 is required
+
+d = {}
+d.fromkeys(l)
+{'a': None, 'b': None, 'c': None}
+d
+{}
+dict.fromkeys(l)
+{'a': None, 'b': None, 'c': None}
+dict.fromkeys(l, True)
+{'a': True, 'b': True, 'c': True}
+
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30, 'Kolkata': 40, 'Punjab': 38, 'kerala': None, 'cochin': 10}
+
+places.pop("cochin")
+10
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30, 'Kolkata': 40, 'Punjab': 38, 'kerala': None}
+places.pop("cochin")
+Traceback (most recent call last):
+  File "<pyshell#79>", line 1, in <module>
+    places.pop("cochin")
+KeyError: 'cochin'
+places.pop("cochin", "key is not present")
+'key is not present'
+
+places.popitem()
+('kerala', None)
+places.popitem()
+('Punjab', 38)
+places
+{'bangalore': 25, 'Mysore': 35, 'chennai': 30, 'Kolkata': 40}
+
+del places["Mysore"]
+places
+{'bangalore': 25, 'chennai': 30, 'Kolkata': 40}
+
+d1 = dict([("a", 1), ("b", 2), ("c", 3)])
+d1
+{'a': 1, 'b': 2, 'c': 3}
+
+{*d1, *places}
+{'chennai', 'Kolkata', 'a', 'c', 'bangalore', 'b'}
+{**d1, **places}
+{'a': 1, 'b': 2, 'c': 3, 'bangalore': 25, 'chennai': 30, 'Kolkata': 40}
+
+d1 | places
+{'a': 1, 'b': 2, 'c': 3, 'bangalore': 25, 'chennai': 30, 'Kolkata': 40}
---
+++ sets_.py
@@ -0,0 +1,324 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+s1 = {2, 4, 6, 8, 10}
+s2 = {3, 6, 9, 12}
+
+# union
+s1.union(s2)
+{2, 3, 4, 6, 8, 9, 10, 12}
+s1
+{2, 4, 6, 8, 10}
+s2
+{9, 3, 12, 6}
+
+# intersection
+s1.intersection(s2)
+{6}
+s1
+{2, 4, 6, 8, 10}
+s2
+{9, 3, 12, 6}
+
+# difference
+s1.difference(s2)
+{8, 2, 10, 4}
+
+s2.difference(s1)
+{9, 3, 12}
+
+s1 - s2
+{8, 2, 10, 4}
+s2 - s1
+{9, 3, 12}
+
+# concatenation
+"hai" + "hello"
+'haihello'
+[1, 2] + [3, 4]
+[1, 2, 3, 4]
+(1, 2) + (3, 4)
+(1, 2, 3, 4)
+
+s1 + s2
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    s1 + s2
+TypeError: unsupported operand type(s) for +: 'set' and 'set'
+
+
+s1.union(s2)
+{2, 3, 4, 6, 8, 9, 10, 12}
+s1
+{2, 4, 6, 8, 10}
+s2
+{9, 3, 12, 6}
+
+
+s1.update(s2)
+s1
+{2, 3, 4, 6, 8, 9, 10, 12}
+s2
+{9, 3, 12, 6}
+
+a = {"google", "gmail"}
+b = {"fb", "insta", "google"}
+
+a.update(b)
+a
+{'gmail', 'google', 'fb', 'insta'}
+
+a = {"google", "gmail"}
+b = ("fb", "insta", "google")
+
+a.update(b)
+a
+{'insta', 'gmail', 'google', 'fb'}
+
+# intersection_update
+a = {"google", "gmail"}
+b = {"fb", "insta", "google"}
+
+a.intersection_update(b)
+a
+{'google'}
+
+# difference_update
+a = {"google", "gmail"}
+b = {"fb", "insta", "google"}
+
+a.difference_update(b)
+a
+{'gmail'}
+
+a = {"google", "gmail"}
+b = {"fb", "insta", "google"}
+b.difference_update(a)
+b
+{'insta', 'fb'}
+a
+{'gmail', 'google'}
+
+
+# symmetric_difference
+a = {"google", "gmail"}
+b = {"fb", "insta", "google"}
+
+a.symmetric_difference(b)
+{'insta', 'gmail', 'fb'}
+
+# symmetric_difference_update
+a = {"google", "gmail"}
+b = {"fb", "insta", "google"}
+
+a.symmetric_difference_update(b)
+a
+{'insta', 'gmail', 'fb'}
+
+
+hash(10)
+10
+hash(1.2)
+461168601842738689
+hash("hai")
+-7009278248679551178
+hash([1, 2, 3])
+Traceback (most recent call last):
+  File "<pyshell#89>", line 1, in <module>
+    hash([1, 2, 3])
+TypeError: unhashable type: 'list'
+hash({1, 2})
+Traceback (most recent call last):
+  File "<pyshell#90>", line 1, in <module>
+    hash({1, 2})
+TypeError: unhashable type: 'set'
+hash((1, 2))
+-3550055125485641917
+
+
+# empty datatypes
+l = list(), []
+s = str(), ""
+t = tuple(), ()
+s = set()
+
+s1 = {}
+type(s1)
+<class 'dict'>
+
+
+# tuple activity
+
+a = (1, )
+
+t1 = (1, 2)
+t2 = (3, 4)
+
+t1 + t2
+(1, 2, 3, 4)
+(*t1, *t2)
+(1, 2, 3, 4)
+
+t = (1, 2, 3, 4, ["hai", "hello", 23], "python")
+t[4]
+['hai', 'hello', 23]
+t[4][0]
+'hai'
+t[4][0][-1]
+'i'
+t[4][0][-1] = "y"
+Traceback (most recent call last):
+  File "<pyshell#118>", line 1, in <module>
+    t[4][0][-1] = "y"
+TypeError: 'str' object does not support item assignment
+
+t = (1, 2, 3, 4, ["hai", "hello", 23], "python")
+t[1:3]
+(2, 3)
+t[1:3] = ["hai", 10]
+Traceback (most recent call last):
+  File "<pyshell#122>", line 1, in <module>
+    t[1:3] = ["hai", 10]
+TypeError: 'tuple' object does not support item assignment
+
+t1 = (1, 2)
+t2 = (3, 4)
+
+l1 = [1, 2]
+l2 = [3, 4]
+[l1, l2]
+[[1, 2], [3, 4]]
+
+t1 = (1, 2)
+t2 = (3, 4)
+[t1, t2]
+[(1, 2), (3, 4)]
+
+(t1, t2)
+((1, 2), (3, 4))
+
+(*t1, *t2)
+(1, 2, 3, 4)
+print((t1, t2))
+((1, 2), (3, 4))
+
+
+# adding elements to a set
+
+b = {"fb", "insta", "google"}
+b.update("gmail")
+b
+{'google', 'm', 'a', 'fb', 'insta', 'i', 'g', 'l'}
+b.update(["gmail"])
+b
+{'gmail', 'google', 'm', 'a', 'fb', 'insta', 'i', 'g', 'l'}
+
+b = {"fb", "insta", "google"}
+b.add(10)
+b
+{'insta', 10, 'google', 'fb'}
+
+b.add("gmail")
+b
+{'gmail', 'google', 10, 'fb', 'insta'}
+
+b.update("gmail")
+b
+{'gmail', 'google', 10, 'm', 'a', 'fb', 'insta', 'i', 'g', 'l'}
+
+b.update(1.25)
+Traceback (most recent call last):
+  File "<pyshell#159>", line 1, in <module>
+    b.update(1.25)
+TypeError: 'float' object is not iterable
+
+b.add(1.25)
+b
+{1.25, 'gmail', 'google', 10, 'm', 'a', 'fb', 'insta', 'i', 'g', 'l'}
+
+b = {"fb", "insta", "google"}
+b.add(1)
+b
+{'insta', 'google', 1, 'fb'}
+b.add(True)
+b
+{'insta', 'google', 1, 'fb'}
+b.add(False)
+b
+{False, 1, 'google', 'fb', 'insta'}
+b.add(0)
+b
+{False, 1, 'google', 'fb', 'insta'}
+
+b.add("fb")
+b
+{False, 1, 'google', 'fb', 'insta'}
+
+b.add([1, 2])
+Traceback (most recent call last):
+  File "<pyshell#177>", line 1, in <module>
+    b.add([1, 2])
+TypeError: unhashable type: 'list'
+
+
+# removing elements from a set
+b
+{False, 1, 'google', 'fb', 'insta'}
+b.remove(False)
+b
+{1, 'google', 'fb', 'insta'}
+b.remove(False)
+Traceback (most recent call last):
+  File "<pyshell#184>", line 1, in <module>
+    b.remove(False)
+KeyError: False
+
+b.discard(False)
+b.discard(1)
+b
+{'google', 'fb', 'insta'}
+
+b.pop()
+'google'
+b.pop()
+'fb'
+b.pop()
+'insta'
+
+b = {'google', 'fb', 'insta'}
+b.clear()
+b
+set()
+id(b)
+2595722448896
+
+del b
+b
+Traceback (most recent call last):
+  File "<pyshell#200>", line 1, in <module>
+    b
+NameError: name 'b' is not defined
+
+s1 = {2, 4, 6, 8, 10}
+s2 = {1, 3, 5}
+s1.intersection(s2)
+set()
+
+s1.isdisjoint(s2)
+True
+s2 = {2, 3}
+s1.isdisjoint(s2)
+False
+
+s1 = {2, 4, 6, 8, 10}
+s2 = {2, 4, 10}
+
+s1.issuperset(s2)
+True
+s2.issuperset(s1)
+False
+
+s2.issubset(s1)
+True
+s1.issubset(s2)
+False
